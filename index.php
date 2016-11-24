10dedf618a7a2181452968178755854fce7edb9c
MRTNS98 <martins.miguel1998@gmail.com>
2016-11-24 08:50:38 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <title></title>
+    </head>
+    <body>
+        
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">Gestionnaire d'evenements</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li>Liste utilisateurs</a></li>
+                </ul>
+            </div>
+        </nav>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
